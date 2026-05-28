460063aa838dc63ffa528832491eb9bfcf343b47
vrudakov1315-art <vrudakov1315@gmail.com>
2026-05-28 14:19:15 +0300

    fix: разделить импорты, добавить валидацию github_url в projects/forms.py

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from .models import Project
 
 
@@ -7,3 +8,11 @@ class ProjectForm(forms.ModelForm):
         model = Project
         fields = ['name', 'description', 'github_url', 'status']
         widgets = {'description': forms.Textarea(attrs={'rows': 5})}
+
+    def clean_github_url(self):
+        url = self.cleaned_data.get('github_url')
+        if url and 'github.com' not in url:
+            raise forms.ValidationError(
+                'Укажите корректную ссылку на github.com'
+            )
+        return url
